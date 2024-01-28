41108ca6f871217de5f5bb73185f45932f1cdc10
MateuszOriol <94061760+MateuszOriol@users.noreply.github.com>
2024-01-29 00:08:26 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const connection = require("./db");
-const userRoutes = require("./routes/users");
 
-connection();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const itemRoutes = require("./routes/item");
+const opinionRoutes = require("./routes/opinions");
+
+const app = express();
 
 app.use(express.json());
-app.use(cors());
 
-app.use("/api/users", userRoutes);
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.use("/api/user", userRoutes);
+app.use("/api/item", itemRoutes);
+app.use("/api/opinions", opinionRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Connected to database & listening on port:",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
